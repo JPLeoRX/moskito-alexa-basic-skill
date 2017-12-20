8017cd050a8c532b6d8ff68a2351c33848c4ea33
jpleorx <leo.ertuna@gmail.com>
2017-12-20 16:53:27 +0200

    Reorganized speechlet

--- src/main/java/moskito/MoskitoSpeechlet.java
+++ src/main/java/moskito/MoskitoSpeechlet.java
@@ -8,23 +8,26 @@ import moskito.speech.MoskitoSpeechletResponse;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.ResourceBundle;
-
 public class MoskitoSpeechlet implements Speechlet, MoskitoSpeechletResponse {
-
     private static final Logger LOGGER = LogManager.getLogger();
 
+    // Default Session methods
+    //------------------------------------------------------------------------------------------------------------------
     @Override
     public void onSessionStarted(SessionStartedRequest sessionStartedRequest, Session session) throws SpeechletException {
         LOGGER.info("onSessionEnded requestId={}, sessionId={}", sessionStartedRequest.getRequestId(), session.getSessionId());
     }
 
-
     @Override
     public void onSessionEnded(SessionEndedRequest sessionEndedRequest, Session session) throws SpeechletException {
         LOGGER.info("onSessionEnded requestId={}, sessionId={}", sessionEndedRequest.getRequestId(), session.getSessionId());
     }
+    //------------------------------------------------------------------------------------------------------------------
 
+
+
+    // Skill methods
+    //------------------------------------------------------------------------------------------------------------------
     @Override
     public SpeechletResponse onLaunch(LaunchRequest launchRequest, Session session) throws SpeechletException {
         LOGGER.info("onLaunch requestId={}, sessionId={}", launchRequest.getRequestId(), session.getSessionId());
@@ -36,40 +39,46 @@ public class MoskitoSpeechlet implements Speechlet, MoskitoSpeechletResponse {
     @Override
     public SpeechletResponse onIntent(IntentRequest intentRequest, Session session) throws SpeechletException {
         LOGGER.info("onIntent requestId={}, sessionId={}", intentRequest.getRequestId(), session.getSessionId());
-        LOGGER.info(intentRequest.getIntent());
-
-        // Redirect into our response, amazon help response, or ask user for input again
-        Intent intent = intentRequest.getIntent();
-        String intentName = (intent != null) ? intent.getName() : null;
 
         // Setup locale
         Responses.initialize(intentRequest.getLocale());
 
-        if ("StatusIntent".equals(intentName))
+        // Redirect into handle intent
+        return handleIntent(intentRequest.getIntent());
+    }
+
+    private SpeechletResponse handleIntent(Intent intent) {
+        // If there is no intent
+        if (intent == null)
+            return getErrorResponse();
+
+        // Status
+        else if ("StatusIntent".equals(intent.getName()))
             return getStatusResponse();
 
-        else if ("ThresholdsIntent".equals(intentName))
+        // Thresholds
+        else if ("ThresholdsIntent".equals(intent.getName()))
             return getThresholdsResponse();
 
-        // If Alerts
-        else if ("AlertsIntent".equals(intentName)) {
+        // Alerts
+        else if ("AlertsIntent".equals(intent.getName())) {
             int number = 1;
             Slot slot = intent.getSlot("Number");
 
-            // If there is such slot
-            if (slot != null)
-                // If there is a value
-                if (slot.getValue() != null)
-                    // We will use this value in our call
-                    number = Integer.valueOf(slot.getValue());
+            if (slot != null)                                                       // If there is such slot
+                if (slot.getValue() != null)                                        // If there is a value
+                    number = Integer.valueOf(slot.getValue());                      // We will use this value in our call
 
             return getAlertsResponse(number);
         }
 
-        else if ("AMAZON.HelpIntent".equals(intentName))
+        // Amazon Help
+        else if ("AMAZON.HelpIntent".equals(intent.getName()))
             return getHelpResponse();
 
+        // If the intent is not recognized
         else
             return getErrorResponse();
     }
+    //------------------------------------------------------------------------------------------------------------------
 }
